f5ab95b63421cab3cd0052b0756665a79d879b22
Lucas Guimarães de Oliveira <lucas.guimaraes.lc@gmail.com>
2020-11-10 23:17:06 -0300

    Consegui fazer Funcionar, mas não segue a ordem matemática, nem aceita os parenteses.

--- calculadoraAv.py
+++ calculadoraAv.py
@@ -1,9 +1,29 @@
-import re
-
+operadores = {
+    "+": lambda op1, op2: op1 + op2,
+    "-": lambda op1, op2: op1 - op2,
+    "*": lambda op1, op2: op1 * op2,
+    "/": lambda op1, op2: op1 / op2
+}
 
 def separador(texto):
-	pass
-
-
+    operador = ("(", ")", "+", "-", "*", "/")
+    numero = ""
+    operador_atual = ""
+    for x in operador:
+        texto = texto.replace(x, " " + x + " ")
+    texto = texto.split(" ")
+    for x in texto:
+        print(x)
+    for x in texto:
+        if x != "+" and x != "-" and x != "*" and x != "/" and numero != "":
+            resultado = operadores[operador_atual](float(numero), float(x))
+            print(resultado)
+            numero = resultado
+        elif x != "+" and x != "-" and x != "*" and x != "/":
+            numero = x
+            print (numero)
+        else:
+            operador_atual = x
+    return resultado
 
 print(separador(input("Digite a equação: ")))
