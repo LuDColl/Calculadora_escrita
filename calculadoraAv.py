9cf06e9f669852afb3aa3359b8d9d6999a4f89c1
Lucas Guimarães de Oliveira <lucas.guimaraes.lc@gmail.com>
2020-11-10 23:47:39 -0300

    Multiplicação ta funfando minha gente.

--- calculadoraAv.py
+++ calculadoraAv.py
@@ -5,15 +5,20 @@ operadores = {
     "/": lambda op1, op2: op1 / op2
 }
 
+
 def separador(texto):
     operador = ("(", ")", "+", "-", "*", "/")
     numero = ""
     operador_atual = ""
+    indice = 0
     for x in operador:
         texto = texto.replace(x, " " + x + " ")
     texto = texto.split(" ")
     for x in texto:
         print(x)
+
+
+    """
     for x in texto:
         if x != "+" and x != "-" and x != "*" and x != "/" and numero != "":
             resultado = operadores[operador_atual](float(numero), float(x))
@@ -24,6 +29,17 @@ def separador(texto):
             print (numero)
         else:
             operador_atual = x
+    """
+    for x in texto:
+        if x == "*":
+            resultado = operadores[x](float(texto[indice - 1]), float(texto[indice + 1]))
+            texto[indice - 1] = resultado
+            del texto[indice]
+            del texto[indice]
+            indice -= 1
+        indice += 1
+    for x in texto:
+        print(x)
     return resultado
 
 print(separador(input("Digite a equação: ")))
