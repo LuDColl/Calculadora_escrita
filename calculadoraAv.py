7f0adaf66ad0bdc1c4cddc8da2987a8b4a903fc4
Lucas Guimarães de Oliveira <lucas.guimaraes.lc@gmail.com>
2021-04-09 00:02:56 -0300

    Reorganizei e comitei que devia faz tempo

--- calculadoraAv.py
+++ calculadoraAv.py
@@ -1,101 +1,117 @@
+# Kernel do algorítimo
 contas = {
     "+": lambda op1, op2: op1 + op2,
-    "-": lambda op1, op2: op1 - op2,
     "*": lambda op1, op2: op1 * op2,
     "/": lambda op1, op2: op1 / op2
 }
 
-
-operadores = ["*", "/"]
+# Todas as listas
 sinais = ["+", "-"]
+operadores = ["*", "/"]
 parenteses = ["(", ")"]
+parenteses_com_operandos = [
+    ")" + x for x in (operadores)] + [
+    x + "(" for x in (sinais + operadores)]
+prioridades = parenteses + operadores + sinais
+numeros = [str(x) for x in range(10)]
+valores = numeros + prioridades
 
-operandos = operadores + sinais
-prioridades = operadores + sinais + parenteses
+
+# Funções Booleanas
+def eh_um_numero(valor):
+    if isinstance(valor, float):
+        return True
+    else:
+        return False
 
 
-def separador(texto):
-    for x in prioridades:
-        texto = texto.replace(x, " " + x + " ")
+# Funções
+def tirar_espacos_duplos(texto):
+    while "  " in texto:
         texto = texto.replace("  ", " ")
-    return texto.strip().split(" ")
+    return texto
+
+print(prioridades)
+def separador(texto):
+    for prioridade in prioridades:
+        texto = texto.replace(prioridade, " " + prioridade + " ")
+    return tirar_espacos_duplos(texto).strip().split(" ")
+
+
+def juntador(texto): return "".join(texto)
+
+
+def numerador(lista):
+    for indice in range(len(lista)):
+        if not lista[indice] in prioridades:
+            lista[indice] = float(lista[indice])
+    return lista
 
 
 def sinalizador(texto):
-    indice = 1
-    while indice < len(texto):
-        if texto[indice] in sinais and texto[indice - 1] in sinais:
-            if texto[indice] == texto[indice - 1]:
-                texto[indice - 1] = "+"
-                del texto[indice]
+    sinais_juntos = [primeiro +
+                     segundo for primeiro in sinais for segundo in sinais]
+    for sinal_junto in sinais_juntos:
+        while sinal_junto in texto:
+            if sinal_junto[0] == sinal_junto[1]:
+                texto = texto.replace(sinal_junto, " ")
             else:
-                texto[indice - 1] = "-"
-                del texto[indice]
-            indice = 1
-            continue
-        if texto[indice] in sinais and texto[indice - 1] in operadores:
-            if texto[indice] == "+":
-                del texto[indice]
-            if texto[indice] == "-" and isinstance(texto[indice + 1], float):
-                del texto[indice]
-                texto[indice] *= -1
-        if texto[0] == "+" and isinstance(texto[1], float):
-            del texto[0]
-            indice = 1
-            continue
-        if texto[0] == "-" and isinstance(texto[1], float):
-            del texto[0]
-            texto[0] *= -1
-            indice = 1
-            continue
-        indice += 1
+                texto = texto.replace(sinal_junto, "-")
+    texto = numerador(separador(texto))
+    for indice in range(len(texto)):
+        anterior_eh_negativo = texto[indice - 1] == "-"
+        if eh_um_numero(texto[indice]) and anterior_eh_negativo:
+            texto[indice] *= -1
+            del texto[indice - 1]
     return texto
 
 
-def numerador(texto):
-    indice = 0
-    while indice < len(texto):
-        if not texto[indice] in prioridades:
-            texto[indice] = float(texto[indice])
-        indice += 1
-    return texto
+def deletador_de_parenteses(valor_lateral):
+    if eh_um_numero(valor_lateral):
+        return "*"
+    else:
+        return valor_lateral
 
 
-def ordenador(texto):
-    indice = len(texto) - 1
-    while indice >= 0:
-        if texto[indice] == "(":
-            del texto[indice]
-            texto[indice] = ordem(sinalizador(texto[indice:]))[0]
-            indice += 1
-            for x in texto[indice:]:
-                if x == ")":
-                    del texto[indice]
-                    texto = sinalizador(texto)
+def ordenador(lista):
+    for indice in range(len(lista) - 1, - 1, - 1):
+        if lista[indice] == "(":
+            for indice_final in range(len(lista[indice + 1:])):
+                if lista[indice_final] == ")":
+                    lista[indice + 1] = ordem(
+                        sinalizador(lista[indice + 1:indice_final - 1]))
+                    del lista[indice + 1:indice_final - 1]
+                    if indice + 1 == len(lista) - 1 or not(
+                        eh_um_numero(lista[indice + 1])):
+                        del lista[indice + 1]
                     break
-                del texto[indice]
-            indice = len(texto) - 1
-        indice -= 1
-    return texto
+    return lista
 
 
 def conta(texto, operador):
     indice = 0
     while indice < len(texto):
-        if indice != 0 and isinstance(texto[indice], float) and isinstance(texto[indice - 1], float):
-            texto[indice - 1] = contas["*"](
+        nao_eh_o_primeiro = indice != 0
+        eh_um_numero = isinstance(texto[indice], float)
+        anterior_eh_um_numero = isinstance(
+            texto[indice - 1], float)
+        soma = nao_eh_o_primeiro and eh_um_numero and anterior_eh_um_numero
+        if soma:
+            texto[indice - 1] = contas["+"](
                 texto[indice - 1], texto[indice])
             del texto[indice]
             indice -= 1
             continue
-        if texto[indice] == ")":
-            if operador == "-":
-                del texto[indice]
-            break
         elif texto[indice] == operador:
-            resultado = contas[texto[indice]](
+            eh_parenteses = operador == "(" or operador == ")"
+            eh_o_ultimo = indice == len(texto) - 1
+            if eh_parenteses and eh_o_ultimo:
+                del texto[indice]
+                break
+            elif eh_parenteses:
+                texto[indice] = "*"
+            texto[indice - 1] = contas[texto[indice]](
                 texto[indice - 1], texto[indice + 1])
-            texto[indice - 1] = resultado
             del texto[indice]
             del texto[indice]
             indice -= 1
@@ -106,39 +122,29 @@ def conta(texto, operador):
 
 def ordem(texto):
     texto = conta(texto, "*")
+    texto = conta(texto, "(")
+    texto = conta(texto, ")")
     texto = conta(texto, "/")
     texto = conta(texto, "+")
     return conta(texto, "-")
 
 
 def calculadoraAv(texto):
-    texto = separador(texto)
-    print("Pós separador:")
-    print(texto)
-    print("")
-
-    texto = numerador(texto)
-    print("Pós numerador")
-    print(texto)
-    print("")
-
     texto = sinalizador(texto)
     print("Pós sinalizador:")
     print(texto)
     print("")
-
     texto = ordenador(texto)
     print("Pós ordenador")
     print(texto)
     print("")
-
+    print("Resposta: ")
     return ordem(texto)[0]
 
 
-numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]
-valores = numeros + prioridades
+print(calculadoraAv(input("Digite a equação: ")))
 
-
+"""
 def existe(texto):
     if texto == "":
         return False
@@ -152,7 +158,7 @@ def decimal(texto):
     indice = 1
     while len(texto) > 2 and indice < len(texto):
         condicao = bool(texto[indice] == "." and (not texto[indice - 1] in numeros or texto[indice - 1]
-            == "." or not texto[indice + 1] in numeros or texto[indice + 1] == "."))
+                                                  == "." or not texto[indice + 1] in numeros or texto[indice + 1] == "."))
         if condicao:
             return False
         indice += 1
@@ -191,6 +197,7 @@ def ordem_dos_parenteses(texto):
 def ordem_dos_operandos(texto):
     indice = 1
     if texto[-1] in operandos:
+        print("F")
         return False
     while indice < len(texto):
         condicao = bool((texto[indice] in operadores and (texto[indice - 1] in operandos or texto[indice - 1] == "("))
@@ -228,3 +235,4 @@ def verificacao(texto):
 
 
 print(calculadoraAv(verificacao(input("Digite a equação: "))))
+"""
